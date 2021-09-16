2ba3af9d860a1df3cae4fee8a429aaa454992f5c
淀川　海都 <90165574+yodogawa-kaito-ta@users.noreply.github.com>
2021-09-16 13:57:15 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,13 +41,13 @@ const app = Vue.createApp({
         ],
         stage2: [
           false, // 2-1
-          // false, // 2-2
+          false, // 2-2
           // false, // 2-3
         ],
         stage3: [
           false, // 3-1
-          // false, // 3-2
-          // false, // 3-3
+          false, // 3-2
+          false, // 3-3
         ]
       },
 
